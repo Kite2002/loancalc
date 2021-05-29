144579a7763f5bc6b1bb4186d8fb9ca98c32a428
Debabrata Batabyal <dbatabyal2018@gmail.com>
2021-05-29 08:47:39 +0530

    First commit

--- main.js
+++ main.js
@@ -1,142 +1,53 @@
-// DEFINE UI VARIABLES
-const taskform = document.querySelector(".taskinput");
-const tasklist = document.querySelector(".collection");
-const clearbtn = document.querySelector(".clear-task");
-const filter = document.querySelector("#filter");
-const taskin = document.querySelector("#task");
-
-//load all event listners
-loadEventListeners();
-
-function loadEventListeners(){
-  document.addEventListener('DOMContentLoaded',getTasks);
-  taskform.addEventListener('submit', addTask);
-  tasklist.addEventListener('click', removeTask);
-  clearbtn.addEventListener('click',clearall);
-  filter.addEventListener('keyup' , filtertask);
-
-}
-
-function getTasks(){
-  let tasks;
-  if(localStorage.getItem('tasks')=== null){
-    tasks = [];
-  }else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
+document.querySelector('.button').addEventListener('click' , calculate);
+const resuts = document.querySelector(".collection");
+function calculate(e){
+  while(resuts.firstChild){
+    resuts.removeChild(resuts.firstChild);
   }
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('intrest');
+  const year = document.getElementById('year');
+  const principal = parseFloat(amount.value);
+  const calcInterest = parseFloat(interest.value)/100/12;
+  const calcPayment = parseFloat(year.value)*12;
 
-  tasks.forEach(function(task){
-  const li = document.createElement('li');
-  li.className = 'collection-item';
-  li.appendChild(document.createTextNode(task));
-  const link = document.createElement('a');
-  link.className = 'delete-item';
-  li.appendChild(link);
-  tasklist.appendChild(li);
+  const x= Math.pow(1+calcInterest,calcPayment);
+  const monthly = (principal*x*calcInterest)/(x-1);
+  if(isFinite(monthly)){
+    const rH = document.createElement('li');
+    rH.className = "collection-item bold";
+    rH.appendChild(document.createTextNode("Results"));
+    const mp = document.createElement('li');
+    mp.className = 'collection-item';
+    const mpHead = document.createElement('b')
+    mpHead.className = 'result-head';
+    mpHead.appendChild(document.createTextNode('Monthly Payment : '))
+    mp.appendChild(mpHead);
+    mp.appendChild(document.createTextNode(monthly.toFixed(2)));
 
-  });
-
-}
-
-
-
-function addTask(e){
-  let tasks;
-  let flag = 0 ;
-  if(localStorage.getItem('tasks')=== null){
-    tasks = [];
-  }else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-  tasks.forEach(function(task){
-    if(taskin.value === task){
-      flag = 1;
-    }
-  })
-  if(flag === 1){
-    alert('Task Already in List')
-  }else{
-    const li = document.createElement('li');
-    li.className = 'collection-item';
-    li.appendChild(document.createTextNode(taskin.value));
-    const link = document.createElement('a');
-    link.className = 'delete-item';
-    li.appendChild(link);
-    tasklist.appendChild(li);
-    storeTaskInLocalStorage(taskin.value);
-    taskin.value = '';
+    
+    const tp = document.createElement('li');
+    tp.className = 'collection-item';
+    const tpHead = document.createElement('b')
+    tpHead.className = 'result-head';
+    tpHead.appendChild(document.createTextNode('Total Payment : '))
+    tp.appendChild(tpHead);
+    tp.appendChild(document.createTextNode((monthly*calcPayment).toFixed(2)));
+    resuts.appendChild(tp)
+    const I = document.createElement('li');
+    I.className = 'collection-item';
+    const IHead = document.createElement('b')
+    IHead.className = 'result-head';
+    IHead.appendChild(document.createTextNode('Monthly Payment : '))
+    I.appendChild(IHead);
+    I.appendChild(document.createTextNode(((monthly*calcPayment)-principal).toFixed(2)));
+    resuts.appendChild(rH)
+    resuts.appendChild(mp)
+    resuts.appendChild(tp)
+    resuts.appendChild(I)
     e.preventDefault();
-
+  }else{
+    alert('Please check your numbers');
   }
-  
+  e.preventDefault ();
 }
-
-
-function storeTaskInLocalStorage(task){
-  let tasks;
-  if(localStorage.getItem('tasks')=== null){
-    tasks = [];
-  }else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-  tasks.push(task);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-
-
-function removeTask(e){
-  if(e.target.classList.contains('delete-item')){
-     e.target.parentElement.remove();
-     removeTaskFromLocalStorage
-     (e.target.parentElement);
-  } 
-}
-
-function removeTaskFromLocalStorage(taskItem){
-  console.log(taskItem);
-  if(localStorage.getItem('tasks')=== null){
-    tasks = [];
-  }else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }  
-  tasks.forEach(function(task,index){
-    if(taskItem.textContent === task){
-      tasks.splice(index, 1);
-
-    }
-  });
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-
-
-function clearall(e){
-  // if(confirm('Remove All')){
-  //   tasklist.innerHTML = '';
-    while(tasklist.firstChild){
-      tasklist.removeChild(tasklist.firstChild);
-    }
-
-    clearTasksFromLocalStorage()
-  }
-
-  function clearTasksFromLocalStorage(){
-    localStorage.clear();
-  }
-
-  function filtertask(e){
-    const text = e.target.value.toLowerCase();
- 
-    document.querySelectorAll('.collection-item').forEach
-    (function(task){
-      const item =task.firstChild.textContent;
-      if(item.toLowerCase().indexOf(text) != -1){
-        task.style.display = 'block';
-      } else{
-        task.style.display = 'none';
-      }
-    });
-  }
-
-
